f2e640d1cfd590dc0930d789f39c74dfddda8a60
Mohab Khaled <mohabk044@gmail.com>
2025-02-06 11:37:47 +0200

    Part 01 Revision

--- Demo/Data/Models/Employee.cs
+++ Demo/Data/Models/Employee.cs
@@ -11,7 +11,7 @@ namespace Demo.Data.Models
 
         public int Id { get; set; }
         public string? Name { get; set; }
-        public double Salary { get; set; }
+        public double Salary { get; set; } 
         public int? Age { get; set; }
 
     }
