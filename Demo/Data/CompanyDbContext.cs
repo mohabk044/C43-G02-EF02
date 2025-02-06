50e1d51b9cdcea39fc66c354c4ab51b0277de95e
Mohab Khaled <mohabk044@gmail.com>
2025-02-06 11:36:13 +0200

    Add project files.

---
+++ Demo/Data/CompanyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data
+{
+    internal class CompanyDbContext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=CompanyG02;Trusted_Connection=True;trustservercertificate=True;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }    
+    }
+}
+
---
+++ Demo/Data/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data.Models
+{
+    internal class Employee
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Salary { get; set; }
+        public int? Age { get; set; }
+
+    }
+}
